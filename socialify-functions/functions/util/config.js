309a9f146259cc25358a7b9446947d76dfb1e244
anshaj <anshaj9elnino@gmail.com>
2020-06-18 22:57:54 +0200

    code refactoring

---
+++ socialify-functions/functions/util/config.js
@@ -0,0 +1,10 @@
+module.exports = {
+    apiKey: "AIzaSyBYNy_75HOLfshUMG6RJY0wVH-Roi34Lmo",
+    authDomain: "socialify-2e968.firebaseapp.com",
+    databaseURL: "https://socialify-2e968.firebaseio.com",
+    projectId: "socialify-2e968",
+    storageBucket: "socialify-2e968.appspot.com",
+    messagingSenderId: "1036685278318",
+    appId: "1:1036685278318:web:cdbf6b67960c5814ccba7f",
+    measurementId: "G-VJ5P4R68HF"
+};
---
+++ socialify-functions/functions/util/fbAuth.js
@@ -0,0 +1,27 @@
+const { admin } = require('./admin');
+
+module.exports = (req, res, next) => {
+    if (req.headers.authorization && req.headers.authorization.startsWith('Bearer ')) {
+        idToken = req.headers.authorization.split('Bearer ')[1];
+    }
+    else {
+        console.error('No Token Found')
+        return res.status(403).json({ error: 'Unauthorized' });
+    }
+    admin.auth().verifyIdToken(idToken)
+        .then(decodedToken => {
+            req.user = decodedToken;
+            return db.collection('users')
+                .where('userId', '==', req.user.uid)
+                .limit(1)
+                .get();
+        })
+        .then(data => {
+            req.user.handle = data.docs[0].data().handle;
+            return next();
+        })
+        .catch(err => {
+            console.error('Error while verifying token', err);
+            return res.status(403).json(err);
+        })
+}
