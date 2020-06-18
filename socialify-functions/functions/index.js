bdb9ac93b7fec79a14dac0b27f0d87b82bb011c9
anshaj <anshaj9elnino@gmail.com>
2020-06-18 20:51:06 +0200

    login route

--- socialify-functions/functions/index.js
+++ socialify-functions/functions/index.js
@@ -166,4 +166,46 @@ app.post('/signup', (req, res) => {
 });
 
 
+//Login Route
+
+app.post('/login', (req, res) => {
+    const user = {
+
+        email: req.body.email,
+        password: req.body.password
+
+    };
+    let errors = {};
+
+    if (isEmpty(user.email)) {
+
+        errors.email = 'Must not be empty';
+    }
+    if (isEmpty(user.password)) {
+
+        errors.password = 'Must not be empty';
+    }
+    if (Object.keys(errors).length > 0)
+        return res.status(400).json(errors);
+
+    firebase.auth().signInWithEmailAndPassword(user.email, user.password)
+        .then(data => {
+            return data.user.getIdToken();
+        })
+        .then(token => {
+            return res.json({ token });
+        })
+        .catch(err => {
+            console.error(err);
+            if (err.code === 'auth/wrong-password') {
+                return res.status(400).json({ general: 'Wrong credential, please try again' });
+            }
+            else {
+                return res.status(500).json({ error: err.code });
+            }
+        })
+
+})
+
+
 exports.api = functions.region('europe-west1').https.onRequest(app);
