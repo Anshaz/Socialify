9568e7062f2d2fb845154b441badda6723426197
anshaj <anshaj9elnino@gmail.com>
2020-06-18 18:31:47 +0200

    express api+authentication

---
+++ socialify-functions/dbschema.js
@@ -0,0 +1,14 @@
+let db = {
+
+    scream: [
+        {
+            userHandle: 'user',
+            body: 'this is a body',
+            createdAt: '2020-06-18T13:45:12:01.018Z',
+            likeCount: 5,
+            commentCount: 2
+        }
+
+    ]
+
+};
--- socialify-functions/functions/index.js
+++ socialify-functions/functions/index.js
@@ -1,8 +1,102 @@
 const functions = require('firebase-functions');
+const admin = require('firebase-admin');
+const app = require('express')();
 
-// // Create and Deploy Your First Cloud Functions
-// // https://firebase.google.com/docs/functions/write-firebase-functions
-//
-// exports.helloWorld = functions.https.onRequest((request, response) => {
-//  response.send("Hello from Firebase!");
-// });
+
+admin.initializeApp();
+
+var config = {
+    apiKey: "AIzaSyBYNy_75HOLfshUMG6RJY0wVH-Roi34Lmo",
+    authDomain: "socialify-2e968.firebaseapp.com",
+    databaseURL: "https://socialify-2e968.firebaseio.com",
+    projectId: "socialify-2e968",
+    storageBucket: "socialify-2e968.appspot.com",
+    messagingSenderId: "1036685278318",
+    appId: "1:1036685278318:web:cdbf6b67960c5814ccba7f",
+    measurementId: "G-VJ5P4R68HF"
+};
+
+const firebase = require('firebase');
+firebase.initializeApp(config);
+
+
+app.get('/screams', (req, res) => {
+
+    admin.firestore()
+        .collection('screams')
+        .orderBy('createdAt', 'desc')
+        .get()
+        .then(data => {
+            let screams = [];
+            data.forEach(doc => {
+                screams.push({
+
+                    screamId: doc.id,
+                    body: doc.data().body,
+                    userHandle: doc.data().userHandle,
+                    createdAt: doc.data().createdAt
+
+                });
+            });
+
+            return res.json(screams);
+        })
+
+        .catch(err => console.error(err));
+
+});
+
+app.post('/scream',(req, res) => {
+    const newScream = {
+
+        body: req.body.body,
+        userHandle: req.body.userHandle,
+        createdAt: new Date().toISOString()
+
+    };
+
+    admin.firestore()
+        .collection('screams')
+        .add(newScream)
+        .then(doc => {
+
+            res.json({ message: `document ${doc.id} created successfully` });
+        })
+        .catch(err => {
+
+            res.status(500).json({ error: 'something went wrong' });
+            console.error(err);
+
+        });
+
+});
+
+//signup route
+
+app.post('/signup', (req, res) => {
+
+    const newUser = {
+        email: req.body.email,
+        password: req.body.password,
+        confirmPassword: req.body.confirmPassword,
+        handle: req.body.handle,
+    };
+
+    //TODO Data validation
+
+    firebase.auth().createUserWithEmailAndPassword(newUser.email, newUser.password)
+        .then((data) => {
+            return res
+                .status(201)
+                .json({ message: `user ${data.user.uid} signed up successfully` });
+
+        })
+        .catch((err) => {
+
+            console.error(err);
+            return res.status(500).json({ error: err.code });
+
+        });
+});
+
+exports.api = functions.region('europe-west1').https.onRequest(app);
