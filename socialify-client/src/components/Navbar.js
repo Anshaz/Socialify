166654bd330612e2457024ac145779ddaa813433
anshaj <anshaj9elnino@gmail.com>
2020-06-25 01:09:08 +0200

    login function added

--- socialify-client/src/components/Navbar.js
+++ socialify-client/src/components/Navbar.js
@@ -3,18 +3,22 @@ import {Link} from 'react-router-dom';
 import { AppBar } from '@material-ui/core';
 import Toolbar from '@material-ui/core/Toolbar';
 import Button from '@material-ui/core/Button';
+import AppIcon from '../images/logo.png';
 
 class Navbar extends Component {
     render() {
         return (
            <AppBar>
                <Toolbar className ="nav-container">
+               <img src = {AppIcon} alt = "logo"/>
+
                    <Button color = "default" component ={Link} to="/Login">
                        Login</Button>
                    <Button color = "default" component ={Link} to="/">
                        Home</Button>
                    <Button color = "default" component ={Link} to ="/signup">
                        Signup</Button>
+
                </Toolbar>
 
            </AppBar>
--- socialify-client/src/components/Scream.js
+++ socialify-client/src/components/Scream.js
@@ -7,7 +7,7 @@ import moment from 'moment';
 import Card from '@material-ui/core/Card';
 import CardContent from '@material-ui/core/CardContent';
 import CardMedia from '@material-ui/core/CardMedia';
-import { Typography } from '@material-ui/core';
+import Typography from '@material-ui/core/Typography';
 
 const styles = {
     card: {
